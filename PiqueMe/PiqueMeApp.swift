68856f7f343aa13c0a15f850a9e39c6083db5da3
Diya Hituvalli <dhituval@umich.edu>
2024-08-21 15:12:40 -0400

    Initial Commit

---
+++ PiqueMe/PiqueMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PiqueMeApp.swift
+//  PiqueMe
+//
+//  Created by Diya Hituvalli on 8/21/24.
+//
+
+import SwiftUI
+
+@main
+struct PiqueMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
